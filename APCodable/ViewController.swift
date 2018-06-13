f42c396c12aaf196fe63c39ccd49474b6b0ab316
user <user@ap-ios.local>
2018-06-13 15:43:26 +0530

    Initial Commit

---
+++ APCodable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APCodable
+//
+//  Created by user on 13/06/18.
+//  Copyright © 2018 Amit Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
