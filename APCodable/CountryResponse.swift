49689bd4c3996cf980bbf2eda867069e62d06c41
user <user@AP-iOS.local>
2018-07-14 18:45:19 +0530

    first commit

---
+++ APCodable/CountryResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CountryResponse.swift
+//  APCodable
+//
+//  Created by user on 13/06/18.
+//  Copyright © 2018 Amit Patel. All rights reserved.
+//
+
+import UIKit
+
+class CountryResponse: NSObject {
+
+}
